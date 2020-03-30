297ef3f1c3bbcce20e3ae7aae30d108969d047e5
Eric Zaporzan <eric.zaporzan@neofinancial.com>
2020-03-30 11:23:44 -0600

    Fixing an issue with profiles containing one or more spaces

--- src/exporter.ts
+++ src/exporter.ts
@@ -5,7 +5,7 @@ import { AWSX_HOME } from './config';
 const EXPORTS_PATH = `${AWSX_HOME}/exports.sh`;
 
 const exportEnvironmentVariables = (profile: string): void => {
-  fs.writeFileSync(EXPORTS_PATH, `export AWS_PROFILE=${profile}`);
+  fs.writeFileSync(EXPORTS_PATH, `export AWS_PROFILE="${profile}"`);
 };
 
 export default exportEnvironmentVariables;
